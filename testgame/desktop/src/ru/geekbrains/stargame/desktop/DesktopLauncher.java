ec81beeaaa59afe06a2243020cc23350da11cb83
pticaperepel <92641116+EugeneBazarova@users.noreply.github.com>
2021-10-17 20:29:49 +0300

    Update DesktopLauncher.java

--- testgame/desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
+++ testgame/desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package ru.geekbrains.stargame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.geekbrains.stargame.StarGame;
+import ru.geekbrains.stargame.StartGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
+		new LwjglApplication(new StartGame(), config);
 	}
 }
